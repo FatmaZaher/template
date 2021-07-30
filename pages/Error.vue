f8b3f498d4724ee4c21b57c5333c8890524e6f54
FatmaZaher <fatoomzaher1999@gmail.com>
2021-07-30 21:58:13 +0300

    add

--- pages/Error.vue
+++ pages/Error.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="">
+  <div class="error">
     <div class="p-5 text-center">
-      <ErrorSvg class="w-50"/>
+      <ErrorSvg />
     </div>
     <p class="text-center my-3">الصفحة المطلوبة غير موجودة</p>
     <div class="text-center my-5">
       <b-button class="btn main-btn">
-        <nuxt-link :to="'/'">
-          الرجوع للرئيسية
-        </nuxt-link>
+        <nuxt-link :to="'/'"> الرجوع للرئيسية </nuxt-link>
       </b-button>
     </div>
   </div>
@@ -19,4 +17,16 @@ export default {
   layout: 'auth',
 }
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.error {
+  svg {
+    width: 50%;
+  }
+  @media (max-width: 767px) {
+    padding: 120px 0;
+    svg {
+      width: 100%;
+    }
+  }
+}
+</style>
